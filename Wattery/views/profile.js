cdcddd418b11b8e78ea0c03bf1178792388893cd
kkotelczuk <konrad.kotelczuk@gmail.com>
2019-06-01 00:03:02 +0200

    feat: add profile simple view

--- Wattery/views/profile.js
+++ Wattery/views/profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import BasicButton from '../components/button';
 
 export default class Profile extends Component {
   constructor(props) {
@@ -8,11 +9,48 @@ export default class Profile extends Component {
     };
   }
 
+  handleLogout = () => {
+    this.props.navigation.navigate('Auth');
+  }
+
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style={styles.container}>
+        <Text style={styles.title}> <Text style={styles.titlePrefix}>Login: </Text>unknown</Text>
+        <BasicButton 
+          title="Change Password" 
+          onPress={() => this.props.navigation.push('Edit')}
+        />
+        <BasicButton 
+          title="Logout" 
+          onPress={this.handleLogout}
+          style={styles.logoutButton}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    padding: 32,
+    justifyContent: 'flex-start',
+  },
+  logoutButton: {
+    marginTop: 16,
+    backgroundColor: '#FF3B30',
+    position: 'absolute',
+    bottom: 12,
+    alignSelf: 'center',
+    width: '100%'
+  },
+  title: {
+    fontSize: 32,
+    padding: 32
+  },
+  titlePrefix: {
+    fontWeight: '300'
+  }
+})
