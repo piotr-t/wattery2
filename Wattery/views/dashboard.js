52563d0a10cfae449738afcb343aed57b8832926
kkotelczuk <konrad.kotelczuk@gmail.com>
2019-05-31 23:06:55 +0200

    feat: add dialog and fill container

--- Wattery/views/dashboard.js
+++ Wattery/views/dashboard.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Modal } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import Dialog from "react-native-dialog";
 
 export default class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      percentage: 0
+      percentage: 40,
+      dialogVisible: false,
     };
   }
+
+  showDialog = () => {
+    this.setState({ dialogVisible: true });
+  };
+
+  handleAddDrink = (type) => {
+     const isUnSafeDrink = type === 'alcohol';
+    const newPercentageValue = (isUnSafeDrink ? -20 : 20) + +this.state.percentage;
+
+    this.setState({ 
+      percentage: newPercentageValue,
+      dialogVisible: false 
+    });
+  };
+
+  handleDelete = () => {
+    this.setState({ dialogVisible: false });
+  };
 
   render() {
     const { percentage } = this.state;
@@ -17,13 +37,27 @@ export default class Dashboard extends Component {
       <View style={styles.container}>
         <Text>Your daily water routine</Text>
         <Text style={styles.title}>{percentage}%</Text>
-        <LinearGradient colors={['#0B5BD3', '#FFF']} style={styles.waterContainer}>
-          <View style={styles.waterPortion}></View>
-          <View style={styles.waterPortion}></View>
-          <View style={styles.waterPortion}></View>
-          <View style={styles.waterPortion}></View>
-          <View style={styles.waterPortion}></View>
-        </LinearGradient>
+        <TouchableOpacity style={styles.waterContainer}
+        onPress={() => {
+            this.showDialog();
+          }}>
+          <LinearGradient colors={['#0B5BD3', '#FFF']} style={{flex: 1}}>
+            <View style={[styles.waterPortion, percentage < 100 && styles.emptyPortion]}></View>
+            <View style={[styles.waterPortion, percentage < 80 && styles.emptyPortion]}></View>
+            <View style={[styles.waterPortion, percentage < 60 && styles.emptyPortion]}></View>
+            <View style={[styles.waterPortion, percentage < 40 && styles.emptyPortion]}></View>
+            <View style={[styles.waterPortion, percentage < 20 && styles.emptyPortion]}></View>
+          </LinearGradient>
+        </TouchableOpacity>
+        <Dialog.Container visible={this.state.dialogVisible} onBackdropPress={() => this.setState({dialogVisible: false})}>
+          <Dialog.Title>Select drink type</Dialog.Title>
+          <Dialog.Description>
+            What did you drink?
+          </Dialog.Description>
+          <Dialog.Button label="A glass of pure Water" onPress={() => this.handleAddDrink('water')} />
+          <Dialog.Button label="Drink with Sugar" onPress={() => this.handleAddDrink('water')} />
+          <Dialog.Button label="Alcohol" onPress={() => this.handleAddDrink('alcohol')} />
+        </Dialog.Container>
       </View>
     );
   }
@@ -48,11 +82,13 @@ const styles = StyleSheet.create({
     flex: 1,
     height: '80%',
     width: '80%',
-    backgroundColor: 'red',
     borderWidth: 4,
   },
   waterPortion: {
     borderWidth: 1,
-    flex: 1
+    flex: 1,
+  },
+  emptyPortion: {
+    backgroundColor: 'white',
   }
 })
